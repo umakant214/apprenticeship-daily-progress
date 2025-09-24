39017d77583163408593ffc9ed420cd38cce1aa9
umakant214 <umakantchandra180@gmail.com>
2025-09-24 19:19:18 +0530

    new updates

--- 0APP C Programming/20-08-2025 Task/incometax.c
+++ 0APP C Programming/20-08-2025 Task/incometax.c
@@ -1,13 +1,46 @@
-#include<stdio.h>
-void main(){
-	//wap to input per month salary of your father and calculate total income tax he need to pay 
-	int m,it;
-	printf("Enter per month salary :");
-	scanf("%d",&m);
+// #include<stdio.h>
+// void main(){
+// 	//wap to input per month salary of your father and calculate total income tax he need to pay
+// 	int m,it;
+// 	printf("Enter per month salary :");
+// 	scanf("%d",&m);
 
-	it=(m*12)*10/100+(m*12)*18/100-(m*12)*2/100;
-	printf("total income :%d",it);
-	
-	
-	
+// 	it=(m*12)*10/100+(m*12)*18/100-(m*12)*2/100;
+// 	printf("total income :%d",it);
+
+// }
+
+#include <stdio.h>
+
+int main()
+{
+	int monthly_salary;
+	float annual_income, tax = 0;
+
+	printf("Enter your father's monthly salary: ");
+	scanf("%d", &monthly_salary);
+
+	annual_income = monthly_salary * 12;
+
+	if (annual_income <= 250000)
+	{
+		tax = 0;
+	}
+	else if (annual_income <= 500000)
+	{
+		tax = (annual_income - 250000) * 0.10;
+	}
+	else if (annual_income <= 1000000)
+	{
+		tax = (250000 * 0.10) + (annual_income - 500000) * 0.20;
+	}
+	else
+	{
+		tax = (250000 * 0.10) + (500000 * 0.20) + (annual_income - 1000000) * 0.30;
+	}
+
+	printf("Annual Income: ₹%.2f\n", annual_income);
+	printf("Income Tax Payable: ₹%.2f\n", tax);
+
+	return 0;
 }
