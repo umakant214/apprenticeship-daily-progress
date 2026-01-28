b83498e82709aa5e26f53c7c0d31dc0f0aa3a6f9
umakant214 <umakantchandra180@gmail.com>
2026-01-28 19:53:11 +0530

    updates file

--- MERN/ExpressJs/My-First-App/index.js
+++ MERN/ExpressJs/My-First-App/index.js
@@ -1,7 +1,40 @@
 import express from "express";
 const app=express();
-const PORT=80000;
-app.listen(PORT,()=>{
+const PORT=8000;
+app.get("/user",(req,res)=>{
+res.send("get go raha hai")
+})
+
+
+//////////////////////
+
+app.use(express.json());
+app.post("/create",(req,res)=>{
+    console.log(req.body);
+    const name=req.body.name;
+    const age=req.body.age;
+    res.send({name,age})
+})
+
+/////////////////////////////////
+app.put("/update",(req,res)=>{
+    res.send("Put chal raha hai")
+})
+
+const createController=(req,res)=>{
+    const result=[{
+        name:"Umakant",
+        method:"delete",
+        code:200
+    }]
+    res.status(200).send(result)
+}
+
+app.delete("/delete",createController)
+
+
+
+app.listen(PORT,()=>{ 
     console.log("server sdkfjwl ksa.fka running ............");
     
 })
